b4b551ca5f31918aa6616d7bc595276cf09ae935
kweku <kweku360@gmail.com>
2016-06-10 09:43:43 +0000

    Setup Commit

---
+++ app/src/main/java/kfive/com/androidnano/NanoHome.java
@@ -0,0 +1,13 @@
+package kfive.com.androidnano;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NanoHome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nano_home);
+    }
+}
