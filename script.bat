65b1ff4fbe8c7f5693ca2eaad682cf7a2ec035ac
kweku <kweku360@gmail.com>
2016-06-20 11:52:53 +0000

    'Backup-Commit=06/20/2016:11:52:29.08'

--- script.bat
+++ script.bat
@@ -1,9 +1,11 @@
 @echo off
 for /F "tokens=2" %%i in ('date /t') do set mydate=%%i
 set mytime=%time%
+setlocal
+cd /d %~dp0
 git add --all
 git commit -m'Backup-Commit=%mydate%:%mytime%'
-git push origin > gitlogs.txt
+git push origin
 echo Backup successfull on %mydate%:%mytime% >> gitlogs.txt
-echo . >> gitlogs.txt
+echo ------------------------- >> gitlogs.txt
 pause
