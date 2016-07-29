41795c9311ab55a131ec5824084b9e85486606a9
kweku <kweku360@gmail.com>
2016-07-29 22:03:15 +0000

    'Backup-Commit=07/29/2016:22:02:06.99'

--- app/src/main/java/kfive/com/androidnano/Animations/Animations.java
+++ app/src/main/java/kfive/com/androidnano/Animations/Animations.java
@@ -1,15 +1,26 @@
 package kfive.com.androidnano.Animations;
 
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import kfive.com.androidnano.R;
 
 public class Animations extends AppCompatActivity {
 
+    ImageView imgv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_animations);
+        imgv = (ImageView)findViewById(R.id.imageView);
+
+        ValueAnimator anim = ObjectAnimator.ofInt(imgv,"rotationX",300,100);
+        anim.setDuration(2000);
+        anim.start();
     }
 }
